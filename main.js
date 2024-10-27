35a197a46a48f25afef1321406e49980a438a554
AdityaSharma42 <adityasharma3@usf.edu>
2024-10-27 01:26:59 -0400

    Handle order submission and display order summary

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-const sizeselector= document.getElementById('size-selector');
+const productSelector= document.getElementById('product-selector');
 const quantityInput= document.getElementById('quantity');
 const totalPriceElement= document.getElementById('total-price');
 const purchaseButton= document.getElementById('purchase-button');
-const orderSummary= document.getElementById('order-summary');
+const orderSummary= document.getElementById('order-summary'); //selecting by ID
 
 function updateTotalPrice(){
-    constproductPrice= parseFloat(selectedOption.getAttribute('data-price'));
-    const quantity= parseInt (quantityInput.value);
-    const totalPrice= productPrice*quantity;
-    totalPriceElement.textContent= `$${totalPrice.toFixed(2)}`;
-productSelector.addEventListener('change', updateTotalPrice);
-quantity.addEventListener('input', updateTotalPrice);
+    const selectedOption= productSelector.options[productSelector.selectedIndex]; //getting the selected option
+    const productPrice= parseFloat(selectedOption.getAttribute('data-price')); //gettimg the price from the option's data attribute
+    const quantity= parseInt (quantityInput.value); //converting the input from a string to an integer
+    const totalPrice= productPrice * quantity; 
+    totalPriceElement.textContent= `$${totalPrice.toFixed(2)}`;//updating the displayed price
 }
+productSelector.addEventListener('change', updateTotalPrice);
+quantityInput.addEventListener('input', updateTotalPrice);//adding the event listeners
+
+
+function handleOrderSubmission(){
+    const selectedOption = productSelector.options[productSelector.selectedIndex].text;
+    const selectedProduct= selectedOption.split('-')[0].trim(); //with the rest of my code, the page was displaying the product name as well as the hyphen and price after it. Using split and trim allows me to remove things I do not want to display
+    const quantity=quantityInput.value; //this gets the quantity value
+    const totalPrice=totalPriceElement.textContent;
+    orderSummary.textContent= `You ordered ${quantity} of the ${selectedProduct} product. Its total price is: ${totalPrice}`;
+}
+purchaseButton.addEventListener('click', handleOrderSubmission); //adding an event listener
+
